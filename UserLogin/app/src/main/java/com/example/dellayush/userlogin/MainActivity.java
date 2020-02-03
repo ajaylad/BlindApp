e46e450e2d1012d499e77ab443555388e8fbd473
ayushsatra17 <ayushsatra@gmail.com>
2020-02-03 17:05:37 +0530

    Login Module for Visually Impaired Or Guardian

---
+++ UserLogin/app/src/main/java/com/example/dellayush/userlogin/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dellayush.userlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void blindLogin(View view){
+
+    }
+
+    public void guardianLogin(View view){
+
+    }
+
+}
